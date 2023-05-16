5bc7e9541f6437d86f1a712e14e3b46bdc3aec8f
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-16 21:55:00 +0300

    Adding Handler Function

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,7 +1,11 @@
 const Button = (props) => {
   const buttonClasses = `p-2 text-lg bg-teal-700 text-white ${props.className}`;
 
-  return <button className={buttonClasses}>{props.children}</button>;
+  return (
+    <button type={props.type || "button"} className={buttonClasses}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 const AddWorker = () => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const [enteredWage, setEnteredWage] = useState("");
+
+  const workerNameHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const wageHandler = (e) => {
+    setEnteredWage(e.target.value);
+  };
+
+  const addWorkerHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName, enteredWage);
+  };
+
   return (
     <Card className="mt-10">
-      <form className="flex flex-col gap-y-2">
+      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
         <label htmlFor="name" className="font-medium">
           Çalışan İsmi
         </label>
@@ -13,6 +31,7 @@ const AddWorker = () => {
           className="max-w-[40rem] w-full mx-auto border p-2"
           placeholder="Çalışan İsmi Giriniz"
           id="name"
+          onChange={workerNameHandler}
         ></input>
         <label htmlFor="wage" className="font-medium">
           Maaş Miktarı
@@ -22,8 +41,11 @@ const AddWorker = () => {
           className="max-w-[40rem] w-full mx-auto border p-2"
           placeholder="Maaş Miktarı Giriniz"
           id="wage"
+          onChange={wageHandler}
         ></input>
-        <Button className="mt-2">Ekle</Button>
+        <Button className="mt-2" type="submit">
+          Ekle
+        </Button>
       </form>
     </Card>
   );
