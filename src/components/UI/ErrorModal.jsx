0f8b43320f9db47d1a55b3a997f1790f988044a5
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-18 12:21:28 +0300

    Adding Root-Portal

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,20 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 
-const ErrorModal = (props) => {
-  const handleConfirm = () => {
-    if (props.onConfirm) {
-      props.onConfirm();
-    }
-  };
+const BackDrop = (props) => {
+  return (
+    <div
+      className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0 left-0"
+      onClick={props.handleConfirm}
+    ></div>
+  );
+};
 
+const ModalOverlay = (props) => {
   return (
     <div className="error-modal">
-      <div
-        className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0"
-        onClick={handleConfirm}
-      ></div>
-      <Card className="w-[36rem] p-0 z-20 bg-gray-300">
+      <Card className="w-[36rem] !p-0 z-20 bg-gray-300">
         <header className="bg-red-700 p-4 rounded-t-xl">
           <h2 className="text-center text-xl text-white">
             {props.error.title}
@@ -22,12 +23,36 @@ const ErrorModal = (props) => {
         </header>
         <section className="p-4">{props.error.message}</section>
         <footer className="p-4 flex justify-end">
-          <Button className="w-32" onClick={handleConfirm}>
+          <Button className="w-32" onClick={props.handleConfirm}>
             Tamam
           </Button>
         </footer>
       </Card>
     </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  const { title, message } = props.error; // Access title and message from props.error
+  const handleConfirm = () => {
+    if (props.onConfirm) {
+      props.onConfirm();
+    }
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop handleConfirm={handleConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          error={{ title, message }}
+          handleConfirm={handleConfirm}/>,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 };
 
